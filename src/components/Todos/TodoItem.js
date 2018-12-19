390a754732bce85ffb0aff8907fd1007be8521dc
vedant <kushal.m@turtlemint.com>
2018-12-19 15:34:16 +0530

    TodoItem component added

--- src/components/Todos/TodoItem.js
+++ src/components/Todos/TodoItem.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React from 'react';
+import { useState } from "react";
+import { useDispatch } from "redux-react-hook";
+import {deleteTodoDelay, updateTodo} from '../../redux/actions/todos';
 
-const TodoItem = ({ title }) => {
+
+const TodoItem = ({ id, title }) => {
+    const [saveMode, setSaveMode] = useState(false);
+    const [inputVal, setInputVal] = useState(title);
+    const dispatch = useDispatch();
+
     return (
-        <li>{title}</li>
+        <li>
+            {saveMode ? (
+                <form onSubmit={ e => {
+                    e.preventDefault();
+                    dispatch(updateTodo(id, inputVal));
+                    setSaveMode(!saveMode);
+                }}>
+                    <input type="text" value={inputVal} onChange={(e) => {
+                        setInputVal(e.target.value);
+                    }}/>
+                    <button type="submit">Save</button>
+                </form>
+            ): (
+                <div>
+                    <span>{title}</span>
+                    <button onClick={() => {
+                        dispatch(deleteTodoDelay(id));
+                    }}>Delete Todo</button>
+                    <button onClick={() => {
+                        setSaveMode(!saveMode);
+                    }}>Update Todo</button>
+                </div>
+            )}
+        </li>
     )
 }
 export default TodoItem;
